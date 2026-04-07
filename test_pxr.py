c2ea6e1b4e2f02a5e2b188be3e129a5f75fb4ae6
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-04-07 10:38:39 -0700

    temp: add arbitrary test

--- test_pxr.py
+++ test_pxr.py
@@ -1,1 +1,7 @@
-from pxr import Gf, Sdf, Usd, UsdGeom, UsdShade, Vt
+import numpy  # noqa: F401
+from pxr import Gf, Sdf, Usd, UsdGeom, UsdShade, Vt  # noqa: F401
+
+
+def test_foo():
+    print("Sdf=", Sdf)
+    raise ValueError("testing")
